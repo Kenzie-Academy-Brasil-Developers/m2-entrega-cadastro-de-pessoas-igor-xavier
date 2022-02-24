259ab671348847e8cb8d6c1e5dd688ab1d04f0b5
Igor-Xavier <igor.oliveiraxavier93@gmail.com>
2022-02-24 12:14:34 -0300

    correcoes feitas, faltando apenas aplicar o metodo a classe pessoa

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,17 @@ const botaoRegistrar = document.getElementById("register-button")
 botaoRegistrar.addEventListener('click', pegarElementos)
 function pegarElementos(event){
     event.preventDefault()
-    const dados = event.target.parentElement
+    let dados = event.target.parentElement
+    if (Number(new Date().getFullYear()) - Number(event.target.parentElement.dataNascimento.value.slice(0,4)) < 18){
+        alert ("Menores de 18 anos não podem se cadastrar")
+        dados = undefined
+    }
     for(let i=0;i<usuarios.length;i++){
         if(usuarios[i].email == dados.email.value){
             alert ("e-mail já existente")
             dados = undefined
         }
-        if (Number(new Date().getFullYear()) - Number(dados.dataNascimento.value.slice(0,4)) < 18){
-            alert ("Menores de 18 anos não podem se cadastrar")
-            dados = undefined
-        }
+
     }
     criarUsuario(dados)
 }
@@ -46,6 +47,8 @@ function criarUsuario(dados){
 }
 let listaAlunos = document.getElementById("lista-de-alunos")
 function addLista(){
+    totalUsuarios.innerText = ""
+    totalUsuarios.innerText = usuarios.length
     listaAlunos.innerHTML = ""
     for(let i=0;i<usuarios.length;i++){
         let item = document.createElement("li")
@@ -56,6 +59,7 @@ function addLista(){
 let select = document.getElementById("cargoOption")
 let btPesquisa = document.getElementById("btn")
 btPesquisa.addEventListener('click', filtrar)
+
 function filtrar(){
     listaAlunos.innerHTML = ""
     let usuariosFiltrados = usuarios.filter(filtrados)
@@ -64,7 +68,7 @@ function filtrar(){
     }
     if(select.value !== 'Todos')
     for(let i=0;i<usuariosFiltrados.length;i++){
-        listaAlunos.innerHTML = ""
+        
         let item = document.createElement("li")
         listaAlunos.appendChild(item)
         item.innerHTML = usuariosFiltrados[i].nome +' '+ usuariosFiltrados[i].sobrenome+' '+ usuariosFiltrados[i].email+' '+ usuariosFiltrados[i].cargo
@@ -73,4 +77,5 @@ function filtrar(){
         addLista()
     }
 }
-
+totalUsuarios = document.getElementById("total-alunos")
+totalUsuarios.innerText += usuarios.length
